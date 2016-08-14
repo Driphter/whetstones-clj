d0258b3dc87d2451279ce88423d14a7368ea2782
Driphter <TheDriphter@gmail.com>
2016-08-14 04:33:55 -0400

    Added restrictions and problems 6-19 to 4clj

--- src/whetstones_clj/4clj/core.clj
+++ src/whetstones_clj/4clj/core.clj
@@ -2,23 +2,36 @@
   (:use clojure.test))
 
 (defn- -trim
-  [s n]
+  [^String s n]
   (subs s n (- (.length s) n)))
+
+(defn- -format-restriction-failure
+  [solution restriction]
+  (str "\r\nsolution:\r\n" (-trim (str solution) 1) "\r\n\r\nrestricted:\r\n" restriction "\r\n"))
+
+(defn- -restriction-test
+  [restriction solution]
+  `(is (nil? (re-find ~(re-pattern (str restriction)) ~(str solution)))
+       ~(-format-restriction-failure solution restriction)))
 
 (defn- -format-failure
   [testform solution]
-  (str "\r\ntest:\r\n" testform "\r\n\r\nsolution:\r\n" (-trim (str solution) 1)))
+  (str "\r\ntest: \r\n" testform "\r\n\r\nsolution: \r\n" (-trim (str solution) 1) "\r\n"))
 
 (defn- -problem-test
   [testform solution]
-  `(is
-    ~(read-string (clojure.string/replace (str testform) #"__" (-trim (str solution) 1)))
-    ~(-format-failure testform solution)))
+  `(is ~(read-string (clojure.string/replace (str testform) #"\b__\b" (-trim (str solution) 1)))
+       ~(-format-failure testform solution)))
 
 (defmacro defproblem
   [number title desc testforms & solution]
-  `(deftest ~(vary-meta (symbol (str "problem-" number))
-                        assoc :prob-title title
-                        :prob-num number
-                        :prob-desc desc)
-     ~@(map #(-problem-test % solution) testforms)))
+  (let [restrictions  (when (map? testforms) (:restrictions testforms))
+        testforms     (if restrictions (first solution) testforms)
+        solution      (if restrictions (rest solution) solution)]
+    `(deftest ~(vary-meta (symbol (str "problem-" number))
+                          assoc :prob-title title
+                          :prob-num number
+                          :prob-desc desc
+                          :prob-restrictions restrictions)
+       ~@(when restrictions (map #(-restriction-test % solution) restrictions))
+       ~@(map #(-problem-test % solution) testforms))))
